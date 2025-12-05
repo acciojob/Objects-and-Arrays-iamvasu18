3f42de05035d9c69859b79bc51cd14840dae967b
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-12-05 22:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+const players = ["John", "Bob", "Alice", "Poppy"];
+const person = {
+  name: "John Doe",
+  age: 80,
+};
+
+// Write your code here
+const team = players;         // Reference to the array
+const team1 = [...players];   // Copy of the array
+const cap1 = { ...person };   // Copy of the object
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
